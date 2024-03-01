6132134939db6f0a0f098cbc924c4ee1efe46faf
thediaax <gabrieldias23.dev@gmail.com>
2024-03-01 01:03:59 -0300

    sla

---
+++ hello_world/HelloWorld.java
@@ -0,0 +1,10 @@
+//import java.lang.String;
+//import java.lang.System;
+//import java.lang;
+import javax.swing.JOptionPane;
+public class HelloWorld{
+    public static void main (String[]args){
+       // System.out.printf("Quero exibir:%s\n",  "Hello World");
+        JOptionPane.showMessageDialog(null, "Hello, World");
+    }
+}
