cf8064e6693e951f1af3417060ab217c23a7e72c
thediaax <gabrieldias23.dev@gmail.com>
2024-03-07 16:29:40 -0300

    07/03

--- soma/Calculadora.java
+++ soma/Calculadora.java
@@ -1,13 +1,7 @@
-import.java.util.Scanner;
-public class Calculadora{
-    public static void main(String...args){
-        var leitor = new Scanner(System.in);
-        double v1, v2, v3;
-        double op;
-        double resultado;
-
-        System.out.println("insira um valor");
-        v1 = leitor.nextDouble();
-
-    }
+Public class Calculadora {
+    // tipo de retorno
+    // nome
+    // lista de parâmetros
+    // corpo
+    
 }
---
+++ soma/TesteCalculadora.java
@@ -0,0 +1,7 @@
+public class TesteCalculadora {
+    
+    public static void main (String [] args){
+
+        Calculadora calc = new Calculadora;
+    }
+}
