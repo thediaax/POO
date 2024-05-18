be9741838ccba1c41c44fd4f67f14897ffbe35b5
thediaax <gabrieldias23.dev@gmail.com>
2024-05-18 20:19:11 -0300

    implementa consulta de codigo em PessoaDAO

--- crud_pessoas/src/App.java
+++ crud_pessoas/src/App.java
@@ -22,48 +22,77 @@ public class App {
                     break;
                 }
                 case 2:{
-                        // 1. pegar os dados juntos ao usuario
-                        var nome = JOptionPane.showInputDialog("Nome?");
-                        var fone = JOptionPane.showInputDialog("Fone?");
-                        var email = JOptionPane.showInputDialog("Email?");
-                        var idade = Integer.parseInt(JOptionPane.showInputDialog("Idade?"));
-                        var codigo = Integer.parseInt(JOptionPane.showInputDialog("Codigo?"));
+                    // var msg = "o que deseja atualizar?\n1- Nome\n2- Fone\n3- Email\n4- Idade";
+                    // JOptionPane.showInputDialog(msg);
+                    // int ops = 0;
+                    // switch (ops) {
+                    //     case 1:{
+                    //         var nome = JOptionPane.showInputDialog("Nome?");
+                    //         break;
+                    //     }
+                            
+                    
+                    //     case 2:{
+                    //         var fone = JOptionPane.showInputDialog("Fone?");
+                    //         break;
+                    //     }
+                    //     case 3:{
+                    //         var email = JOptionPane.showInputDialog("Email?");
+                    //         break;
+                    //     }
+
+                    //     case 4:{
+                    //         var idade = Integer.parseInt(JOptionPane.showInputDialog("Idade?"));
+                    //         break;
+                    //     }
+                    //     case 5:{
+                            
+                    //     }
+                    // }
+                    // 1. pegar os dados juntos ao usuario
+                    var nome = JOptionPane.showInputDialog("Nome?");
+                    var fone = JOptionPane.showInputDialog("Fone?");
+                    var email = JOptionPane.showInputDialog("Email?");
+                    var idade = Integer.parseInt(JOptionPane.showInputDialog("Idade?"));
+                    var codigo = Integer.parseInt(JOptionPane.showInputDialog("Codigo?"));
                         
-                        // 2. construir um objeto pessoa
-                        var p = new Pessoa(codigo, nome, fone, email, idade);
-                        // 3. construir um objeto dao
-                        var dao = new PessoaDAO();
-                        // 4. atualizar usando dao
-                        dao.atualizar(p);
-                        // 5. confirmar atualização
-                        JOptionPane.showMessageDialog(null, "Atualização OK!");
+                    // 2. construir um objeto pessoa
+                    var p = new Pessoa(codigo, nome, fone, email, idade);
+                    // 3. construir um objeto dao
+                    var dao = new PessoaDAO();
+                    // 4. atualizar usando dao
+                    dao.atualizar(p);
+                    // 5. confirmar atualização
+                    JOptionPane.showMessageDialog(null, "Atualização OK!");
                     break;
                 }
                 case 3:{
-                var codigo = Integer.parseInt(JOptionPane.showInputDialog("Codigo?"));
-                var p = new Pessoa(codigo);
-                var dao = new PessoaDAO();
-                dao.deletar(p);
-                JOptionPane.showMessageDialog(null, "Remoção OK!");
-                break;
+                    var codigo = Integer.parseInt(JOptionPane.showInputDialog("Codigo?"));
+                    var p = new Pessoa(codigo);
+                    var dao = new PessoaDAO();
+                    dao.deletar(p);
+                    JOptionPane.showMessageDialog(null, "Remoção OK!");
+                    break;
                 }
                 case 4:{
-                var pessoas = new PessoaDAO().listar();
-                var sb = new StringBuilder("");
-                for(var p: pessoas){
-                    sb.append(p).append("\n");
-                }
-                JOptionPane.showMessageDialog(null, sb);
-                break;
-                }
+                    var pessoas = new PessoaDAO().listar();
+                    var sb = new StringBuilder("");
+                    for (var p : pessoas){
+                      sb.append(p).append("\n");
+                    }
+                    JOptionPane.showMessageDialog(null, sb);
+                    break;
+                  }
                 case 5:{
+                    break;
 
                 }
                 case 6:{
-
+                    break;
                 }
                 case 0:{
-                    
+                    JOptionPane.showMessageDialog(null, "Até mais!");
+                    break;
                 }
             }
         }
--- crud_pessoas/src/PessoaDAO.java
+++ crud_pessoas/src/PessoaDAO.java
@@ -2,42 +2,54 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 public class PessoaDAO {
-    public void cadastrar(Pessoa p) throws Exception{
-        
-        //Clausula catch or declare
-        
-        //Especificar o comando SQL
+    public void cadastrar(Pessoa p) throws Exception {
+
+        // Clausula catch or declare
+
+        // Especificar o comando SQL
         var sql = "INSERT INTO tb_pessoa(nome, fone, email, idade) VALUES(?, ?, ?, ?)";
 
-        //Estabelecer uma conexao com o banco
+        // Estabelecer uma conexao com o banco
         Connection conexao = ConnectionFactory.getConnection();
 
-        //Preparar o comando
+        // Preparar o comando
         PreparedStatement ps = conexao.prepareStatement(sql);
 
-        //Substituir os eventuais placeholders
+        // Substituir os eventuais placeholders
         ps.setString(1, p.getNome());
         ps.setString(2, p.getFone());
         ps.setString(3, p.getEmail());
         ps.setInt(4, p.getIdade());
 
-        //Executar o comando
+        // Executar o comando
         ps.execute();
 
-        //Fechar a conexao
+        // Fechar a conexao
         ps.close();
         conexao.close();
     }
-    public void atualizar(Pessoa p) throws Exception{
+
+    public boolean consultarCod(Pessoa p) throws Exception {
+        var sql = "SELECT 1 FROM tb_pessoa WHERE cod_pessoa = ?";
+        try (
+                var conexao = ConnectionFactory.getConnection();
+                var ps = conexao.prepareStatement(sql);) {
+            ps.setInt(1, p.getCodigo());
+            try (var rs = ps.executeQuery()) {
+                return rs.next();
+            }
+        }
+    }
+
+    public void atualizar(Pessoa p) throws Exception {
         // 1. Especificar comando sql
         var sql = "UPDATE tb_pessoa SET nome?, fone=?, email=?, idade=? WHERE cod_pessoa = ?";
         // 2. Estabelecer uma conexao com o SGBD
-        //try with resources
-        try(
-            var conexao = ConnectionFactory.getConnection();
-            // 3. Preparar o comando
-            var ps = conexao.prepareStatement(sql);
-        ){
+        // try with resources
+        try (
+                var conexao = ConnectionFactory.getConnection();
+                // 3. Preparar o comando
+                var ps = conexao.prepareStatement(sql);) {
             // 4. Substituir os eventuais placeholders
             ps.setString(1, p.getNome());
             ps.setString(2, p.getFone());
@@ -47,22 +59,22 @@ public class PessoaDAO {
             // 5. Executar o comando
             // 6. fechar os recursos
         }
-    
+
     }
+
     public void deletar(Pessoa p) throws Exception {
         // 1. Especificar o comando SQL
 
         var sql = "DELETE FROM tb_pessoa WHERE cod_pessoa = ?";
-        
+
         try (
                 // 2. Estabelecer uma conexao com o SGBD
                 var conexao = ConnectionFactory.getConnection();
 
                 // 3, Preparar o comando
-                var ps = conexao.prepareStatement(sql);
-            ) {
-           
-                 // 4. Substituir os eventuais placeholders
+                var ps = conexao.prepareStatement(sql);) {
+
+            // 4. Substituir os eventuais placeholders
             ps.setInt(1, p.getCodigo());
 
             // 5. Executar o comando
@@ -73,36 +85,33 @@ public class PessoaDAO {
         }
 
     }
-    public java.util.List<Pessoa> listar() throws Exception{
+
+    public java.util.List<Pessoa> listar() throws Exception {
         // 1. Construir uma lista
         var pessoas = new java.util.LinkedList<Pessoa>();
-        // 2. Especificar o comando SQL 
+        // 2. Especificar o comando SQL
         var sql = "SELECT * FROM tb_pessoa";
-        try(
-            // 3. Estabelecer conexao como SGBD
-            var conexao = ConnectionFactory.getConnection();
-            // 4. Preparar o comando
-            var ps = conexao.prepareStatement(sql);
-            // 5. Substituir os eventuais placeholders
-            // 6. Executar o comando
-            java.sql.ResultSet rs = ps.executeQuery();
-
-        ){
-        // 7. Lidar com o resultado
-        while(rs.next()){
-            var pessoa = new Pessoa(
-            rs.getInt("cod_pessoa"),
-            rs.getString("nome"),
-            rs.getString("fone"),
-            rs.getString("email"),
-            rs.getInt("Idade")
-            );
-            pessoas.add(pessoa);
+        try (
+                // 3. Estabelecer uma conexão com o SGBD
+                var conexao = ConnectionFactory.getConnection();
+                // 4. Preparar o comando
+                var ps = conexao.prepareStatement(sql);
+                // 5. Substituir os eventuais placeholders (não tem nenhum)
+                // 6. Executar o comando
+                java.sql.ResultSet rs = ps.executeQuery();) {
+            // 7. Lidar com o resultado
+            while (rs.next()) {
+                var pessoa = new Pessoa(
+                        rs.getInt("cod_pessoa"),
+                        rs.getString("nome"),
+                        rs.getString("fone"),
+                        rs.getString("email"),
+                        rs.getInt("idade"));
+                pessoas.add(pessoa);
+            }
+            // 8. Fechar os recursos (o try-with-resources já faz)
+            // 9. Devolver a lista
+            return pessoas;
         }
-        // 8.fechar os recursos(o try with resources ja faz)
-        // 9. Devolver a lista
-        return pessoas;
-        }
-
     }
 }
